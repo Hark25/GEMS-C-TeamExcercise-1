4b1bc400a12d6ad058a34b9e7546bc589d23b370
Hark25 <harklit25@gmail.com>
2022-09-22 20:59:41 -0700

    added some comments

--- subtraction.java
+++ subtraction.java
@@ -1,12 +1,12 @@
 public class subtraction {
     public double subtract(String equation) throws Exception {
-        String numbers[] = equation.split("-", 2); // Split the string being input
-        double answer;
+        String numbers[] = equation.split("-", 2); // Split the string being input into two parts, limit to only two splits.
+        double answer; //initilize answer
 
         double FirstNum = Double.parseDouble(numbers[0].trim()); // Parse the numbers from the String
         double SecondNum = Double.parseDouble(numbers[1].trim());
-        answer = FirstNum-SecondNum; // Find the answer
-        
-        return answer;
+
+        answer = FirstNum-SecondNum; // Find the answer by subtracting SecondNum from FirstNum
+        return answer; //return final value
     } 
 }
